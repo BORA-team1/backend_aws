f99fea998c2c1e0b23942cf97a06977b39587e7f
julia-heo <100216331+julia-heo@users.noreply.github.com>
2023-09-05 23:26:16 +0900

    20230905

--- config/docker/entrypoint.prod.sh
+++ config/docker/entrypoint.prod.sh
@@ -1,5 +1,10 @@
+# #!/bin/sh
+
+# python manage.py collectstatic --no-input
+
+# exec "$@"
+
+
 #!/bin/sh
-
-python manage.py collectstatic --no-input
-
+python manage.py collectstatic --no-input echo "Apply database migrations" python manage.py migrate
 exec "$@"
