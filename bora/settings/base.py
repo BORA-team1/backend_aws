860608375526ff13541a49c0d56997d50e0b16ed
chaerin <100216331+julia-heo@users.noreply.github.com>
2023-09-06 19:59:14 +0900

    Update base.py

--- bora/settings/base.py
+++ bora/settings/base.py
@@ -17,8 +17,8 @@ from datetime import timedelta
 
 AUTH_USER_MODEL = 'account.User'
 
-import pymysql  
-pymysql.install_as_MySQLdb()
+# import pymysql  
+# pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
